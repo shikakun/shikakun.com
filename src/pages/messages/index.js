28903411aeb0a24a1923645fedad5c17a0d5aba6
shikakun <shikakun@shikakun.com>
2020-06-23 20:34:35 +0900

    :sparkles: styling form

--- src/pages/messages/index.js
+++ src/pages/messages/index.js
@@ -11,18 +11,18 @@ export default class Index extends React.Component {
               <div className="page__content">
                 <form name="direct-messages" method="post" action="https://direct-message-to-shikakun.netlify.app/">
                   <input type="hidden" name="form-name" value="direct-messages" />
-                  <div>
-                    <label>Name: <input type="text" name="name" required /></label>
-                  </div>
-                  <div>
-                    <label>Email: <input type="email" name="email" required /></label>
-                  </div>
-                  <div>
-                    <label>Message: <textarea name="message" required></textarea></label>
-                  </div>
-                  <div>
-                    <button type="submit">Send</button>
-                  </div>
+                  <span class="form-control">
+                    <label>name<input type="text" name="name" required /></label>
+                  </span>
+                  <span class="form-control">
+                    <label>email<input type="email" name="email" required /></label>
+                  </span>
+                  <span class="form-control">
+                    <label>message<textarea name="message" required></textarea></label>
+                  </span>
+                  <span class="form-control">
+                    <button type="submit">send to @shikakun</button>
+                  </span>
                 </form>
               </div>
             </div>
