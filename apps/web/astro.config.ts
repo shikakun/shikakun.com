3f55bbe2f848c1a405350e0f57f88c7093203515
shikakun <shikakun@shikakun.com>
2026-04-21 08:24:05 +0900

    静的サイトには@astrojs/cloudflareアダプターがなくても動作するため削除した

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,9 +1,7 @@
-import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  adapter: cloudflare(),
   integrations: [mdx()],
 });
