430687212bf59e8b43329f040e0841e721811764
shikakun <shikakun@shikakun.com>
2026-04-23 19:55:21 +0900

    FormattedDateをAstroコンポーネントからReactコンポーネントへ置き換えた

--- apps/web/astro.config.ts
+++ apps/web/astro.config.ts
@@ -1,7 +1,8 @@
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  integrations: [mdx()],
+  integrations: [mdx(), react()],
 });
