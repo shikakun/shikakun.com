876ab47106cfb8a2111f37b8167d28a6133f76d7
shikakun <shikakun@shikakun.com>
2026-04-24 15:35:10 +0900

    テスト環境をjsdomへ変更した

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'node',
+    environment: 'jsdom',
   },
   build: {
     lib: {
