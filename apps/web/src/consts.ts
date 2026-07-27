ea2a8ad0bdfc3e902db9d58d29f2160e45e047d3
shikakun <shikakun@shikakun.com>
2026-07-28 08:42:20 +0900

    Google Analyticsの測定IDを変更した

--- apps/web/src/consts.ts
+++ apps/web/src/consts.ts
@@ -3,4 +3,4 @@ export const SITE_DESCRIPTION = 'Software Designer / Poet';
 export const SITE_LANG = 'ja';
 export const SITE_REGION = 'JP';
 export const SITE_X_ID = 'shikakun';
-export const GA_MEASUREMENT_ID = 'G-7024GG473Q';
+export const GA_MEASUREMENT_ID = 'G-60T2BK3GM9';
